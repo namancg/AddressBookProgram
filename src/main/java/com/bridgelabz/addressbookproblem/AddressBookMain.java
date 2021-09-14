3052d77b7328e32a15277b4f28f97e137511334a
namancg <naman.c@perfios.com>
2021-09-14 19:20:28 +0530

    REFACTORED: taking input and displaying

--- src/main/java/com/bridgelabz/addressbookproblem/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbookproblem/AddressBookMain.java
@@ -1,10 +1,48 @@
 package com.bridgelabz.addressbookproblem;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		
 		System.out.println("PERFORMING ADDRESS BOOK OPERATIONS");
+		PersonContact person = new PersonContact();
+		Address address = new Address();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("First Name: ");
+		String firstName = sc.next();
+		
+		System.out.println("Last Name: ");
+		String lastName = sc.next();
+		
+		System.out.println("Phone Number: ");
+		long phoneNumber = sc.nextLong();
+		
+		System.out.println("Email: ");
+		String email = sc.next();
+		
+		System.out.println("City: ");
+		String city = sc.next();
+		
+		System.out.println("State: ");
+		String state = sc.next();
+		
+		sc.close();
+		
+		person.setFirstName(firstName);
+		person.setLastName(lastName);
+		person.setnumber(phoneNumber);
+		person.setEmail(email);
+		address.setCity(city);
+		address.setState(state);
+		person.setAddress(address);
+		
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact(person);
+		addressBook.displayContents();
+
 	}
 
 }
