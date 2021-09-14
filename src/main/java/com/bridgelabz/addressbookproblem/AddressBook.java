7fcc9f7171a4a659e10c5e0813cdbd25f0fb7413
namancg <naman.c@perfios.com>
2021-09-14 19:19:35 +0530

    REFACTORED: addd new contact

--- src/main/java/com/bridgelabz/addressbookproblem/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbookproblem/AddressBook.java
@@ -1,7 +1,26 @@
 package com.bridgelabz.addressbookproblem;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class AddressBook {
-	ArrayList <PersonContact> contactList = new ArrayList<PersonContact>();
+
+public  class AddressBook implements AddressBookIF{
+	
+	ArrayList <PersonContact> list = new ArrayList<PersonContact>();
+	public void addContact(PersonContact person) {
+		list.add(person);
+		
+	}
+	
+	@Override
+	public void displayContents() {
+		
+		Iterator<PersonContact> iterator = list.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+	}
+
+	
 }
